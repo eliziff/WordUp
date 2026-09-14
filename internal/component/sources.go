2bef735f0cc3f0c0f22fea8f28ff19105dd1f856
eliziff <279039582+eliziff@users.noreply.github.com>
2026-09-14 07:03:40 -0600

    Clean up dynamic form shells

--- internal/component/sources.go
+++ internal/component/sources.go
@@ -39,7 +39,19 @@ End Sub
 const formShellSource = `Attribute VB_Name = "WordUpFormShell"
 Option Explicit
 Public Sub WU_ShowForm(ByVal formName As String)
-    VBA.UserForms.Add(formName).Show
+    Dim instance As Object, failure As Long, failureSource As String, failureText As String
+    On Error GoTo Failed
+    Set instance = VBA.UserForms.Add(formName)
+    instance.Show
+CleanUp:
+    On Error Resume Next
+    If Not instance Is Nothing Then Unload instance
+    On Error GoTo 0
+    If failure <> 0 Then Err.Raise failure, failureSource, failureText
+    Exit Sub
+Failed:
+    failure = Err.Number: failureSource = Err.Source: failureText = Err.Description
+    Resume CleanUp
 End Sub
 `
 
